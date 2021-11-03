2260643ad29a463bddec858b1fe3bdf9ec2568dc
mobak88 <78964465+mobak88@users.noreply.github.com>
2021-11-03 14:28:18 +0100

    Finish card component

--- src/components/styles/Background.styled.jsx
+++ src/components/styles/Background.styled.jsx
@@ -2,11 +2,16 @@ import styled from 'styled-components';
 
 const StyledBackground = styled.div`
   height: 100vh;
-  width: 100vw;
   background-color: ${(props) => props.theme.colors.paleBlue};
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (min-width: 1440px) {
+    background-image: url('src/assets/pattern-background-desktop.svg');
+  }
+  background-repeat: no-repeat;
+  background-color: ${(props) => props.theme.colors.paleBlue};
 `;
 
 export default StyledBackground;
