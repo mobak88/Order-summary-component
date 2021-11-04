884ef418ab8651577bc5484454e6f8e589699f09
mobak88 <78964465+mobak88@users.noreply.github.com>
2021-11-04 11:59:48 +0100

    Change background to cover the whole width

--- src/components/styles/Background.styled.jsx
+++ src/components/styles/Background.styled.jsx
@@ -9,6 +9,7 @@ const StyledBackground = styled.div`
 
   @media (min-width: 1440px) {
     background-image: url('/assets/pattern-background-desktop.svg');
+    background-size: 100% 45%;
   }
   background-repeat: no-repeat;
   background-color: ${(props) => props.theme.colors.paleBlue};
